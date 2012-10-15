c680ccd7ecd595960d372bfce74e490957f74c14
GekM <eugene@GekM.(none)>
2012-10-15 14:26:06 +0400

    CG commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cg::Application.config.secret_token = 'c95c5a1604c7082288c30ec325d44327dcab25a23ce01725eb35a3a3d0ef655b99bfa3f5d5025bf793349a0a9c0fe6c6e17a84c1ff1ad3dd3bb63961c53c2754'
